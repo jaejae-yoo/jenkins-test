b06aa8b2d7bfdc2aaff5ecfd082035e1d380c719
jaejae-yoo <wotj102@gmail.com>
2022-08-07 14:44:52 +0900

    feat: 테스트 추가

---
+++ src/test/java/com/example/jenkinstest/TempTest.java
@@ -0,0 +1,22 @@
+package com.example.jenkinstest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class TempTest {
+
+    @Test
+    void test1() {
+        int number = 1;
+
+        assertThat(number).isEqualTo(1);
+    }
+
+    @Test
+    void test2() {
+        int number = 1;
+
+        assertThat(number).isEqualTo(2);
+    }
+}
